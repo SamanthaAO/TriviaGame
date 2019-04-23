1bd1cd13e873535030ebdd9be9e3d94839a981f3
SamanthaAO <samanthaaorcutt@gmail.com>
2019-04-23 14:47:19 -0700

    created variables.

---
+++ assets/javascript/app.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+    var corrrect = 0;
+    var incorrect = 0;
+    var noResponse = 0;
+
+
+    var triviaQuestions = [
+        {
+            question: "1?",
+            a: "A1",
+            b: "B1",
+            c: "C1",
+            d: "D1",
+            answer: c,
+        },
+
+        {
+            question: "2?",
+            a: "A2",
+            b: "B2",
+            c: "C2",
+            d: "D2",
+            answer: a,
+        },
+
+        {
+            question: "3?",
+            a: "A3",
+            b: "B3",
+            c: "C3",
+            d: "D3",
+            answer: b,
+        },
+
+        {
+            question: "4?",
+            a: "A4",
+            b: "B4",
+            c: "C4",
+            d: "D4",
+            answer: a,
+        },
+
+        {
+            question: "5?",
+            a: "A5",
+            b: "B5",
+            c: "C5",
+            d: "D5",
+            answer: d,
+        },
+
+    ];
+
+
+
+})
