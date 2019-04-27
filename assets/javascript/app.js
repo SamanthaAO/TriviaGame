573b6c4466dfe34845607184446f29a0aa34dee6
SamanthaAO <samanthaaorcutt@gmail.com>
2019-04-26 19:27:23 -0700

    added theme elements and updated questions to have trivia questions and relevant images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,59 +17,59 @@ $(document).ready(function () {
     //question array of objects
     var triviaQuestions = [
         {
-            question: "1?",
-            a: "A1",
-            b: "B1",
-            c: "C1",
-            d: "D1",
-            answer: "C1",
-            answerImage: "assets/images/goat.jpg",
-            answerImageAlt: "goat",
+            question: "1. What are the words of House Stark?",
+            a: "A1: Is mighty cold up here",
+            b: "B1: Winter is coming",
+            c: "C1: Only the pack survives the winter",
+            d: "D1: King of the north",
+            answer: "B1: Winter is coming",
+            answerImage: "assets/images/Q1.jpeg",
+            answerImageAlt: "Winter is coming",
 
         },
 
         {
-            question: "2?",
-            a: "A2",
-            b: "B2",
-            c: "C2",
-            d: "D2",
-            answer: "A2",
-            answerImage: "assets/images/goat.jpg",
-            answerImageAlt: "goat",
+            question: "2. What animal is in Petyr Baelish's sigil?",
+            a: "A2: A mockingbird",
+            b: "B2: A weasel",
+            c: "C2: A little finger",
+            d: "D2: A middle finger",
+            answer: "A2: A mockingbird",
+            answerImage: "assets/images/Q2.jpg",
+            answerImageAlt: "Little Finger is dead joke",
         },
 
         {
-            question: "3?",
-            a: "A3",
-            b: "B3",
-            c: "C3",
-            d: "D3",
-            answer: "B3",
-            answerImage: "assets/images/goat.jpg",
-            answerImageAlt: "goat",
+            question: "3. Finish the saying: The night is dark...",
+            a: "A3: and swarming with white walkers",
+            b: "B3: and full of terror",
+            c: "C3: and full of dragons",
+            d: "D3: so lets drink by the fire with giant's bane",
+            answer: "B3: and full of terror",
+            answerImage: "assets/images/Q3.jpg",
+            answerImageAlt: "The Red Lady joke",
         },
 
         {
-            question: "4?",
-            a: "A4",
-            b: "B4",
-            c: "C4",
-            d: "D4",
-            answer: "A4",
-            answerImage: "assets/images/goat.jpg",
-            answerImageAlt: "goat",
+            question: "4: In Which of these cities did Daenerys Targaryen free people?",
+            a: "A4: Astapor",
+            b: "B4: Volantis",
+            c: "C4: Qarth",
+            d: "D4: Pentos",
+            answer: "A4: Astapor",
+            answerImage: "assets/images/Q4.jpg",
+            answerImageAlt: "Daenerys joke",
         },
 
         {
-            question: "5?",
-            a: "A5",
-            b: "B5",
-            c: "C5",
-            d: "D5",
-            answer: "D5",
-            answerImage: "assets/images/goat.jpg",
-            answerImageAlt: "goat",
+            question: "What are House Lannister's words?",
+            a: "A5: Family First",
+            b: "B5: I wouldn't trust me if I were you",
+            c: "C5: A Lannister Always Pays His Debts",
+            d: "D5: Hear Me Roar!",
+            answer: "D5: Hear Me Roar!",
+            answerImage: "assets/images/Q5.jpg",
+            answerImageAlt: "Lanister joke",
         },
 
     ];
@@ -122,11 +122,11 @@ $(document).ready(function () {
         var questionText = "";
         triviaQuestions.forEach(function (questions, i) {
             questionText += `<div id= "question${i}">
-                        <h2> ${questions.question}</h2>
-                        <button id="${questions.a}"class = "m-2 questionButton">${questions.a}</button> <br>
-                        <button id="${questions.b}"class = "m-2 questionButton">${questions.b}</button> <br>
-                        <button id="${questions.c}"class = "m-2 questionButton">${questions.c}</button> <br>
-                        <button id="${questions.d}"class = "m-2 questionButton">${questions.d}</button> <br>
+                        <h2 class="mb-5"> ${questions.question}</h2>
+                        <button id="${questions.a}"class = "m-2 questionButton btn btn-dark btn-block">${questions.a}</button> <br>
+                        <button id="${questions.b}"class = "m-2 questionButton btn btn-dark btn-block">${questions.b}</button> <br>
+                        <button id="${questions.c}"class = "m-2 questionButton btn btn-dark btn-block">${questions.c}</button> <br>
+                        <button id="${questions.d}"class = "m-2 questionButton btn btn-dark btn-block">${questions.d}</button> <br>
                         </div>`;
 
             time = 5;
@@ -152,7 +152,7 @@ $(document).ready(function () {
             //the -5 is becuase currently the answer pages count as no response.
             noResponse = noResponse - 5;
             $("#results").append("<h2> You have reached the end of the game. Congratulations!</h2><div>Correct Answers: " + correct + "</div> <br> <div>Incorrect Answers: " + incorrect + "</div> <br> <div>No Response: " + noResponse + "</div>");
-            $("#results").append("<button type='button' class='btn btn-primary mt-5' id='restartButton'>Restart Game</button>");
+            $("#results").append("<button type='button' class='btn btn-dark mt-5' id='restartButton'>Restart Game</button>");
             $("#timeLeft").hide();
             $("#correct").hide();
         }
@@ -166,7 +166,7 @@ $(document).ready(function () {
             answerText += `<div id= "answer${i}">
         <h2 id = "correct-incorrect"> </h2>
         <div class = "m-2">The answer is: ${answers.answer}</div> <br>
-        <img src="${answers.answerImage}" alt="${answers.answerImageAlt}" width="150" height="150">
+        <img src="${answers.answerImage}" alt="${answers.answerImageAlt}">
         </div>`;
 
             time = 3;
@@ -185,6 +185,7 @@ $(document).ready(function () {
     //on start button atarts on first question slide
     $("#startButton").on("click", function () {
         $("#startButton").remove();
+        $("#throne").remove();
 
         // displays question 1
         displayQuestion(i);
